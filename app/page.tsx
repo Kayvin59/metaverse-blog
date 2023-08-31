b3a46775c1a217678daf5676fd5a9a29db6dd4a0
Kayvin <kayvin4@hotmail.com>
2023-08-31 02:56:32 +0200

    style: add card for post preview

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const data: BlogPost[] = await getData();
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-10">
+    <main className="flex min-h-screen flex-col items-center p-5">
       <h1 className="text-2xl font-bold mb-4">Metaverse Blog</h1>
       <PostList posts={data} />
     </main>
--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,13 +1,19 @@
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
+import { ArrowUpRight } from 'lucide-react';
+
 export default function PostList({ posts }: { posts: BlogPost[] }) {
     return (
         <ul>
             {posts.map((post: BlogPost) => (
-                <li key={post.id} className="flex">
-                    <div>
-                        <h2>{post.title}</h2>
-                        <span>{post.author}</span>
-                    </div>
-                    <p>{post.body}</p>
+                <li key={post.id} className="mb-5 group">
+                    <Card className="flex flex-col p-5 relative">
+                        <ArrowUpRight className="absolute transition-transform transform duration-300 top-4 right-4 group-hover:top-2 group-hover:right-2"/>
+                        <CardHeader className="mb-5">
+                            <CardTitle>{post.title}</CardTitle>
+                            <CardDescription>{post.author}</CardDescription>
+                        </CardHeader>
+                        <CardContent className="line-clamp-2 flex-nowrap p-0">{post.body}</CardContent>
+                    </Card>
                 </li>
             ))}
         </ul>
