653abbea0b8223fbcdfd9e46d94d1c767fb7ea4e
Kayvin <kayvin4@hotmail.com>
2023-09-01 16:24:50 +0200

    feat: add create post link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import PostList from "@/components/PostList";
+import Link from "next/link";
 import { getPosts } from "@/lib/get-data";
+import PostList from "@/components/PostList";
+import { buttonVariants } from "@/components/ui/button";
 
 export default async function Home() {
   const data: BlogPost[] = await getPosts();
@@ -7,6 +9,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen max-w-5xl flex-col items-center mx-auto my-0 p-5 font-roboto">
       <h1 className="text-4xl underline font-bold mb-8 font-cambria">Metaverse Blog</h1>
+      <Link className={`self-end mb-3 bg-foreground font-bold text-white ${buttonVariants({ variant: "outline"})}`} href="/posts/new">New Post</Link>
       <PostList posts={data} />
     </main>
   )
